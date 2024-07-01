2f24dfcad22bf953ffe119733a98cdf03ec95891
eternaltyro <eternaltyro@protonmail.ch>
2024-07-01 14:45:17 +0100

    Upgrade Nixos config from 23.11 to 24.05

--- configuration/etc/nixos/configuration.nix
+++ configuration/etc/nixos/configuration.nix
@@ -1,36 +1,38 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
- 
+
 { config, pkgs, ... }:
- 
+
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
- 
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
- 
+
   networking.hostName = "nixos"; # Define your hostname.
+  networking.fqdn = "nixos.eternaltyro.local";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
- 
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
- 
+
   # Enable networking
   networking.networkmanager.enable = true;
- 
+  networking.nameservers = ["9.9.9.9" "1.1.1.1"];
+
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
- 
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_IN";
- 
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_IN";
     LC_IDENTIFICATION = "en_IN";
@@ -42,26 +44,26 @@
     LC_TELEPHONE = "en_IN";
     LC_TIME = "en_IN";
   };
- 
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
- 
+
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
- 
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
+    variant = "dvorak";
     layout = "us";
-    xkbVariant = "dvorak";
   };
- 
+
   # Configure console keymap
   console.keyMap = "dvorak";
- 
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
- 
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -73,15 +75,15 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
- 
+
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
- 
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
- 
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.eternaltyro = {
     isNormalUser = true;
@@ -97,10 +99,10 @@
       tmux
     ];
   };
- 
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
- 
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -109,11 +111,11 @@
     vim
     linuxKernel.packages.linux_6_1.virtualboxGuestAdditions
   ];
- 
+
   environment.variables = rec {
     SUDO_EDITOR = "vim";
   };
- 
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -121,9 +123,9 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
- 
+
   # List services that you want to enable:
- 
+
   services.resolved.enable = true;
   services.resolved.dnssec = "allow-downgrade";
   services.resolved.fallbackDns = [
@@ -133,16 +135,16 @@
     "2620:fe::9"
   ];
   
- 
+
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
- 
+  services.openssh.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
- 
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -150,7 +152,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
- 
+
   virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
 }
+
+
